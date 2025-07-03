5c232ae5826d824ec047c3d6097605304acdaa26
Adrián Vigil <insightvigil@gmail.com>
2025-07-03 06:24:09 -0600

    Backennd terminado

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -10,6 +10,28 @@ const bcrypt = require('bcrypt-nodejs')
 
 const cors = require('cors')
 
+//Paquete knex que permite una conexión a una base de datos
+const knex = require('knex')
+
+const db = knex({
+client: 'pg',
+connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'midvigil',
+    password: 'toor',
+    database: 'smart-brain',
+},
+});
+
+/*
+db.select('*').from('users').then(data => {
+    console.log(data)
+})
+*/
+
+
+
 app.use(express.json())
 app.use(cors())
 
@@ -46,81 +68,81 @@ app.get('/', (req,res)=> {
 })
 
 app.post('/signin',(req,res) => {
-    // Load hash from your password DB.
-    bcrypt.compare("JOd",'$2a$10$m.XsLiyiN4myK6FdRFWZDOkl077k3Dz723V/cyiUqWLoDqxNYaCA2'
-, function(err, res) {
-        // res == true
-        console.log('first guess',res)
+    db.select('email', 'hash').from('login')
+    .where('email','=', req.body.email)
+    .then(data=> {
+        const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
+        if(isValid) {
+            console.log(isValid)
+            return db.select('*').from('users')
+                .where('email','=',req.body.email)
+                .then(user => {
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('Unable to get user'))
+        }
+        else {
+            console.log(isValid)
+            res.status(400).json('wrong credentials')
+        }  
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+
     });
 
-    
-    bcrypt.compare("veggies", "$2a$10$m.XsLiyiN4myK6FdRFWZDOkl077k3Dz723V/cyiUqWLoDqxNYaCA2", function(err, res) {
-        // res = false
-        console.log('second guess', res)
-    });
-
-
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password ){
-    res.json('SUCCESS')
-    }
-
-    else {
-        console.log(req.body.email)
-        console.log(req.body.password)
-        console.log(database.users[0])
-        res.status(400).json('Error Login in');
-    }
-})
 
 app.post('/register', (req,res) => {
     const {email, name, password} = req.body
-    
-    bcrypt.hash(password, null, null, function(err, hash) {
-    // Store hash in your password DB.
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user=> {res.json(user[0]);
 
-    console.log(hash);
-    });
-    
-    database.users.push({
-        id: '3',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-
-    res.json(database.users[database.users.length-1])
-
+    .catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req,res) => {
     const { id } = req.params;
-    let found =false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
-    })
-        if (!found) {
-            res.status(400).json('not found')
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length) {
+            res.json(user[0])
         }
+        else {
+            res.status(400).json('User not found')
+        }
+    })
+       
 })
 
 app.put('/image', (req,res) => {
     const { id } = req.body;
-    let found =false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        } 
+    db('users')
+    .where('id','=',id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries => { 
+        response.json(entries[0].entries)
+    .catch(err => { res.status(400).json('Unable to count')})
     })
-        if (!found) {
-            res.status(400).json('not found')
-        }
     
 })
 
@@ -140,3 +162,21 @@ app.listen(5001, () => {
 /profile:userId --> GET = user
 /image --> PUT --> user
 */
+
+/*
+.insert({
+  // If you are using Knex.js version 1.0.0 or higher this 
+  // now returns an array of objects. Therefore, the code goes from:
+  // loginEmail[0] --> this used to return the email
+  // TO
+  // loginEmail[0].email --> this now returns the email
+     email: loginEmail[0].email, // <-- this is the only change!
+     name: name,
+     joined: new Date()
+})
+     */
+
+/*
+     //true
+    bcrypt.compareSync("veggies",hash);//false
+    */
